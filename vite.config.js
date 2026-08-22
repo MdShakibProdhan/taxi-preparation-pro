a36bf66cd36b887fce78087eb51e8c3f6de332b0
Md Shakib Prodhan <58662852+MdShakibProdhan@users.noreply.github.com>
2026-08-22 03:24:10 +0300

    Update base URL for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/taxi-exam-app/', // GitHub Pages base path
+  base: '/taxi-preparation-pro/', // GitHub Pages base path
 })
